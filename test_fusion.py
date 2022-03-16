36a55930626c44143e58da45bf9eb20b3f241174
Moritz <mo.hesche@gmail.com>
2022-03-16 21:38:05 +0100

    handler in main file

--- test_fusion.py
+++ test_fusion.py
@@ -1,27 +1,84 @@
-import traceback
+import adsk.core, adsk.fusion, traceback
 
-import adsk.core, adsk.fusion
+handlers = []
+cmd = None
+ctrl = None
+ui = None
+
+
+class TestfusionCreatedHandler(adsk.core.CommandCreatedEventHandler):
+    def notify(self, eventArgs: adsk.core.CommandCreatedEventArgs):
+        print("started TestfusionCreatedHandler")
+        try:
+            inputChangedHandler = TestfusionInputChangedHandler()
+            handlers.append(inputChangedHandler)
+            eventArgs.command.inputChanged.add(inputChangedHandler)
+
+            executeHandler = TestfusionExecuteHandler()
+            handlers.append(executeHandler)
+            eventArgs.command.execute.add(executeHandler)
+
+            eventArgs.command.commandInputs.addBoolValueInput(
+                "testfusionBoolInputId", "bool input", True
+            )
+        except:
+            if ui:
+                ui.messageBox(traceback.format_exc())
+
+
+class TestfusionInputChangedHandler(adsk.core.InputChangedEventHandler):
+    def notify(self, eventArgs: adsk.core.InputChangedEventArgs):
+        print("started TestfusionInputChangedHandler")
+        try:
+            command = eventArgs.firingEvent.sender
+            cmdInput = eventArgs.input
+
+        except:
+            if ui:
+                ui.messageBox(traceback.format_exc())
+
+
+class TestfusionExecuteHandler(adsk.core.CommandEventHandler):
+    def notify(self, eventArgs: adsk.core.CommandEventArgs):
+        print("started TestfusionExecuteHandler")
+        try:
+            command = eventArgs.firingEvent.sender
+
+        except:
+            if ui:
+                ui.messageBox(traceback.format_exc())
 
 
 def run(context):  # pylint:disable=unused-argument
-    ui = adsk.core.Application.get().userInterface
     try:
-        pass
+        global ui
+        global cmd
+        global ctrl
+
+        app: adsk.core.Application = adsk.core.Application.get()
+        ui = app.userInterface
+
+        ws = ui.workspaces.itemById("FusionSolidEnvironment")
+        tab = ws.toolbarTabs.itemById("ToolsTab")
+        panel = tab.toolbarPanels.itemById("SolidScriptsAddinsPanel")
+        cmd = ui.commandDefinitions.addButtonDefinition(
+            "testfusioncommandid", "testfusion", ""
+        )
+        ctrl = panel.controls.addCommand(cmd)
+
+        onCreated = TestfusionCreatedHandler()
+        cmd.commandCreated.add(onCreated)
+        handlers.append(onCreated)
 
     except:
-        msg = "Failed:\n{}".format(traceback.format_exc())
         if ui:
-            ui.messageBox(msg)
-        print(msg)
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
 
 
 def stop(context):  # pylint:disable=unused-argument
-    ui = adsk.core.Application.get().userInterface
-
     try:
-        pass
+        ctrl.deleteMe()
+        cmd.deleteMe()
     except:
-        msg = "Failed:\n{}".format(traceback.format_exc())
         if ui:
-            ui.messageBox(msg)
-        print(msg)
+            ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
